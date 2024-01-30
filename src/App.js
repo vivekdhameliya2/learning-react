e287531bc5dc5235ea46981f82781ebf1abbbc9e
Vivek Dhameliya <64520575+vivekdhameliya2@users.noreply.github.com>
2024-01-29 19:38:08 -0500

    Decrement done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
-let i = 0;
+let i = 0
 export default function App() {
-  
   const [data, setData] = useState(i);
 
-  function updateData() {
-    setData(i++);
+  function Increament() {
+    setData(i++)
   }
+
+  function Decrement() {
+    setData(i--)
+  }
+
   return (
     <div className="App">
       <h1>{data}</h1>
-      <button onClick={updateData}>Increament</button>
+      <button onClick={Increament}>Increament</button>
+      <button onClick={Decrement}>Decrement </button>
     </div>
   );
 }
