6ee3e6edc7d175f04fdb0f7760679d6881d872cd
Vivek Dhameliya <64520575+vivekdhameliya2@users.noreply.github.com>
2024-01-29 19:33:31 -0500

    conter app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
-
+let i = 0;
 export default function App() {
-const [data,setData]= useState("Vivek Dhameliya")
+  
+  const [data, setData] = useState(i);
 
-
-function updateData(){
-    setData ("Hey! How are you?")
-}
+  function updateData() {
+    setData(i++);
+  }
   return (
     <div className="App">
       <h1>{data}</h1>
-      <button  onClick={updateData}>Update data</button>
+      <button onClick={updateData}>Increament</button>
     </div>
   );
 }
